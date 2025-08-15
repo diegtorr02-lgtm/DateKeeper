f1c61c9541a2b682e5b6e7c0d43cb9f51ff99dbd
baechled@herrenkencht.com <BaechleD@herrenknecht.com>
2025-08-15 22:31:35 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,6 @@ import '@ionic/vue/css/display.css';
 /* @import '@ionic/vue/css/palettes/dark.class.css'; */
 import '@ionic/vue/css/palettes/dark.system.css';
 
-/* Theme variables */
 import './theme/variables.css';
 
 const app = createApp(App)
